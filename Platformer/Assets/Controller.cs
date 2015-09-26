701eec509548f4ce5cbaa254bca3e823b8447ea5
bloomspin <contact@bloomspin.com>
2015-09-25 23:11:08 -0400

    Controller Script Added
    Added Controller Script

--- Platformer/Assets/Controller.cs
+++ Platformer/Assets/Controller.cs
@@ -1,8 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public static class Controller {
-    public static bool UP = false;
-    public static bool DOWN = false;
-    public static bool LEFT = false;
+[SerializePrivateVariables]
+public class Controller : MonoBehaviour {
+	public bool UP = false;
+	public bool DOWN = false;
+	public bool LEFT = false;
+	public bool RIGHT = false;
+	public bool JUMP = false;
+
+	void Awake() {
+		DontDestroyOnLoad (this);
+	}
+
+	void Update(){
+		if (Input.GetKeyDown ("up")) {
+			UP = true;
+		}
+		if (Input.GetKeyUp ("up")) {
+			UP = false;
+		}
+		if (Input.GetKeyDown ("down")) {
+			DOWN = true;
+		}
+		if (Input.GetKeyUp ("down")) {
+			DOWN = false;
+		}
+		if (Input.GetKeyDown ("left")) {
+			LEFT = true;
+		}
+		if (Input.GetKeyUp ("left")) {
+			LEFT = false;
+		}
+		if (Input.GetKeyDown ("right")) {
+			RIGHT = true;
+		}
+		if (Input.GetKeyUp ("right")) {
+			RIGHT = false;
+		}
+		if (Input.GetKeyDown ("space")) {
+			JUMP = true;
+		}
+		if (Input.GetKeyUp ("space")) {
+			JUMP = false;
+		}
+	}
 }
