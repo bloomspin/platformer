652d1b41bbbaf757b1e9d5fde5164bc4f937823f
FallenWyvern <b.holmes.29@gmail.com>
2015-09-25 22:56:44 -0400

    movimg

---
+++ Platformer/Assets/Controller.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Controller {
+    public static bool UP = false;
+    public static bool DOWN = false;
+    public static bool LEFT = false;
+}
