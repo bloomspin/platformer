36a194eb79a572bda89b3cada747ec75a9059fed
bloomspin <contact@bloomspin.com>
2015-09-26 00:43:45 -0400

    Build1
    UI & Player Control

--- Platformer/Assets/_Control.cs
+++ Platformer/Assets/_Control.cs
@@ -7,6 +7,7 @@ public class _Control : MonoBehaviour {
 	Controller MainScript;
 	Rigidbody2D hitbox;
 	DateTime jumpStart = DateTime.Now;
+	float maxSpeed = 4.0f;
 	public bool jumping = false;
 
 	// Use this for initialization
@@ -27,9 +28,33 @@ public class _Control : MonoBehaviour {
 			TimeSpan diff = now - jumpStart;
 
 			if (diff.TotalMilliseconds < 100) {
-				hitbox.AddForce (new Vector2 (7, 30));
+				hitbox.AddForce (new Vector2 (0, 30));
 			}
 		}
+
+		if (MainScript.RIGHT) {
+	
+			hitbox.AddForce (new Vector2 (3, 0));
+			if (this.gameObject.transform.rotation.y != 0){
+			this.gameObject.transform.Rotate(new Vector3(0,180,0));
+			}
+
+			hitbox.velocity = Vector3.ClampMagnitude(hitbox.velocity, maxSpeed);
+
+		}
+
+		if (MainScript.LEFT) {
+			if (this.gameObject.transform.rotation.y != 180){
+			this.gameObject.transform.Rotate(new Vector3(0,180,0));
+			}
+			hitbox.AddForce (new Vector2 (-3, 0));
+			
+			hitbox.velocity = Vector3.ClampMagnitude(hitbox.velocity, maxSpeed);
+			
+		}
+
+
+
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
